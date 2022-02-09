f542cdce0368f2f7fc5b5aeec3be103ed0e6b5e9
Yash Dixit <yashdixitsq@gmail.com>
2022-02-09 16:14:02 +0530

    docs: for logger module

--- src/utils/logger/index.js
+++ src/utils/logger/index.js
@@ -5,6 +5,12 @@ var log4js = require("log4js");
 
 var logger = log4js.getLogger();
 
+
+/* 
+ Sets the logger level according to the environment.
+ If the environment is local we use debug. We don't use console.log everywhere. 
+*/
+
 if (config.environment == "local") {
     logger.level = "debug"
 } else {
